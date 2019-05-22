083bb25b0bc52a40818e32f9684ce7be528f966f
maxthedude <markus.klausz@gmail.com>
2019-05-22 19:16:32 +0200

    reading json data from a http stream

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = require('./ServiceAccountKey.json');
 const app = express();
+const http = require('http');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -12,6 +13,37 @@ admin.initializeApp(functions.config().firebase);
 
 let db = admin.firestore();
 
+app.get('/importData', (req, res) => {
+	// read data from https://jsonplaceholder.typicode.com/users and add the users to the users collection
+	let jsonValues = readData();
+	if (jsonValues != null)
+		res.status(200).send('Json Data read successfully');
+	else {
+		res.status(400).send('Something went wrong');
+	}
+});
+
+function readData() {
+		req = http.get("http://jsonplaceholder.typicode.com/users", function(res) {
+		let data = '',
+			json_data;
+	
+		res.on('data', function(stream) {
+			data += stream;
+		});
+		res.on('end', function() {
+		json_data = JSON.parse(data);
+	
+			// will output a Javascript object
+			console.log(json_data);
+			return json_data;
+		});
+	});
+	
+	req.on('error', function(e) {
+			console.log(e.message);
+	});
+}
 
 
 app.get('/timestamp', (req, res) => {
@@ -25,9 +57,9 @@ app.get('/timestamp-cached', (req, res) => {
 
 app.get('/dbaccess/writeStaticUser', (req, res) => {
 	console.log('called dbaccess');
-	var aTuringRef = db.collection('users').doc('aturing');
+	let aTuringRef = db.collection('users').doc('aturing');
 
-	var setAlan = aTuringRef.set({
+	let setAlan = aTuringRef.set({
 		'first': 'Alan',
 		'middle': 'Mathison',
 		'last': 'Turing',
@@ -41,9 +73,9 @@ app.get('/dbaccess/writeStaticUser', (req, res) => {
 
 app.get('/dbaccess/writeUser/:username/:email/:password', (req, res) => {
 	console.log('called dbaccess');
-	var userRef = db.collection('users').doc(req.params.username);
+	let userRef = db.collection('users').doc(req.params.username);
 	console.log(req.params);
-	var setAlan = userRef.set({
+	let setAlan = userRef.set({
 		'username': req.params.username,
 		'email': req.params.email,
 		'password': req.params.password
@@ -52,20 +84,15 @@ app.get('/dbaccess/writeUser/:username/:email/:password', (req, res) => {
 	}).catch(errors => console.error({ errors }));
 
 });
-const user = {
-	firstname: 'test',
-	lastname: 'test'
-} // des wär a json object
 
-//JSON.stringify(user) // würd aus dem des machen {firstname:"test",lastname:"test"} - also wirklich als string und ned mehr als json
 
 /**
  * Simple request that extracts data from firebase firestore (database)
  */
 app.get('/userProfile/:username', (req, res) => {
 	//get the users collection
-	var docRef = db.collection("users");
-	var users = [];
+	let docRef = db.collection("users");
+	let users = [];
 	//.doc(req.params.username);
 	var query = docRef.where('username', '==', req.params.username).get()
 		.then(snapshot => {
@@ -77,12 +104,40 @@ app.get('/userProfile/:username', (req, res) => {
 				console.log(doc.id, ' =>', doc.data());
 				users.push(doc.data());
 			});
-			res.send().json(JSON.stringify(users));
+			res.send((JSON.stringify(users)));
 
 		}).catch(err => {
 			console.error({err});
 		});
 	});
+
+	/**
+	 * reads all users from the corresponding collection
+	 */
+	app.get('/getAllUsers', (req, res) => {
+		//get the users collection
+		var docRef = db.collection("users");
+		var users = [];
+		//.doc(req.params.username);
+		var query = docRef.get()
+			.then(snapshot => {
+				if (snapshot.empty) {
+					users.push('No Users in Database');
+				}
+	
+				snapshot.forEach(doc => {
+					console.log(doc.id, ' =>', doc.data());
+					users.push(doc.data());
+				});
+				res.json(users);
+	
+			}).catch(err => {
+				console.error({err});
+			});
+		});
+	
+
+
 	// Create and Deploy Your First Cloud Functions
 	// https://firebase.google.com/docs/functions/write-firebase-functions
 
